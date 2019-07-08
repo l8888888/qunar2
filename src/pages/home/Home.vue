36fff5bb192f756f07c67df25bb8dfbaec2a0578
wayne <379984646@qq.com>
2019-07-09 01:39:07 +0800

    add homeicons

--- src/pages/home/Home.vue
+++ src/pages/home/Home.vue
@@ -2,17 +2,20 @@
   <div>
     <homeheader></homeheader>
     <homeswiper></homeswiper>
+    <homeicons></homeicons>
   </div>
 </template>
 
 <script>
 import homeheader from './component/homeheader'
 import homeswiper from './component/swiper'
+import homeicons from './component/icons'
 export default {
   name: 'Home',
   components: {
     homeheader,
-    homeswiper
+    homeswiper,
+    homeicons
   }
 }
 </script>
---
+++ src/pages/home/component/icons.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="icons">
+        <div class="icons-item">
+            <div class="icons-img"></div>
+            <div class="icons-content"></div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'homeicons'
+}
+</script>
+
+<style lang="stylus" scoped>
+
+</style>
